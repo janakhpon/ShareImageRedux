5bde66992ccdb0243b199b8782a00e94a17009a2
janakhpon <jnovaxer@gmail.com>
2020-02-02 08:35:14 +0630

    GDEV005

---
+++ src/store.js
@@ -0,0 +1,12 @@
+//USER TYPES
+export const USER_SIGNUP = 'USER_SIGNUP'
+export const USER_SIGNIN = 'USER_SIGNIN'
+export const USER_SIGNUP_ERROR = 'USER_SIGNUP_ERROR'
+export const USER_SIGNIN_ERROR = 'USER_SIGNIN_ERROR'
+export const USER_RESET = 'USER_RESET'
+export const USER_REMOVE = 'USER_REMOVE'
+export const GET_USERS = 'GET_USERS'
+export const GET_ME = 'GET_ME'
+
+//POST TYPES
+export const GET_LIST
