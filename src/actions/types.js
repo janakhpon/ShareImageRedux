d60d0c7c98ce4674912798219a7969c0b0e68bf1
janakhpon <jnovaxer@gmail.com>
2020-02-02 08:54:27 +0630

    GDEV007

---
+++ src/actions/types.js
@@ -0,0 +1,14 @@
+//USER TYPES
+export const USER_SIGNUP = 'USER_SIGNUP'
+export const USER_SIGNIN = 'USER_SIGNIN'
+export const USER_UPDATE = 'USER_UPDATE'
+export const USER_SIGNUP_ERROR = 'USER_SIGNUP_ERROR'
+export const USER_SIGNIN_ERROR = 'USER_SIGNIN_ERROR'
+export const USER_UPDATE_ERROR = 'USER_UPDATE_ERROR'
+export const USER_RESET = 'USER_RESET'
+export const USER_REMOVE = 'USER_REMOVE'
+export const GET_USERS = 'GET_USERS'
+export const GET_ME = 'GET_ME'
+
+//POST TYPES
+
